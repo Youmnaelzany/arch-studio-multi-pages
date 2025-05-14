1e8508d1b996bdb6f41455179ec26275cb2b51bb
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-05-14 21:47:50 +0300

    Remove unused components and clean up About and Contact pages

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,11 +1,9 @@
-import OurHeritage from "./ourHeritage";
 
 export default function About() {
   return (
     <div>
       <h1>About</h1>
       <p>This is the About page.</p>
-      <OurHeritage />
     </div>
   );
 }
